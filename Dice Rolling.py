a091aacdd63163511f440e76ad42b032e517d172
Amirhossein Payab <89769597+amirhosseinpayab@users.noreply.github.com>
2022-09-13 15:25:05 +0430

    Add files via upload

---
+++ Dice Rolling.py
@@ -0,0 +1,21 @@
+import random
+user = input("\nDo you want to rolling dice (y or n) ? ")
+if user == 'y' or user == 'n':
+    while user == 'y':
+        dice = [1,2,3,4,5,6]
+        computer = random.choice(dice)
+        print(computer)
+        user = input("Do you want agian (y or n) ? ")
+        if user == 'n':
+            print('\nGood bye :)\n')
+            break
+else:
+        print('You are type wrong ! please do agian ...')
+        
+
+
+
+
+                    
+
+
